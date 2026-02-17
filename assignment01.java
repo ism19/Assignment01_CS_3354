e8ef6b85e8338f0580cc7c1bdbf70af3eeba3734
Ismah Hassan <ismah@Ismahs-Air.lan>
2026-02-16 21:18:29 -0600

    added arrays

---
+++ assignment01.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class assignment01 {
+    public static void main(String[] args) {
+        String[] itemNames = new String[10];
+        double[] itemPrices = new double[10];
+        int[] itemStocks = new int[10];
+    }
+
+    
+}
