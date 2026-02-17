d8ff6afd0a632197310c2a3189abf66768ce0536
Ismah Hassan <ismah@Ismahs-Air.lan>
2026-02-16 23:06:26 -0600

    added menu feature

--- assignment01.java
+++ assignment01.java
@@ -5,6 +5,33 @@ public class assignment01 {
         String[] itemNames = new String[10];
         double[] itemPrices = new double[10];
         int[] itemStocks = new int[10];
+
+        while (true) {
+            Scanner input = new Scanner(System.in);
+
+            System.out.println("Enter 1-3 to choose an option:");
+            System.out.println("1 for View");
+            System.out.println("2 for Restock");
+            System.out.println("3 to Exit");
+            
+            int option = input.nextInt();
+
+            switch (option) {
+                case 1:
+                    printInventory(itemNames, itemPrices, itemStocks);
+                    break;
+                case 2:
+                    System.out.println("Enter the name of the item to restock: ");
+                    String item = input.next();
+                    System.out.println("Enter the amount to restock: ");
+                    int amount = input.nextInt();
+                    restockItem(itemNames, itemStocks, item, amount);
+                    break;
+                case 3:
+                    System.out.println("Exiting program.");
+                    return;
+            }
+        }
     }
 
     public static void printInventory(String[] names, double[] prices, int[] stocks) {
